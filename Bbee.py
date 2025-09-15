d4f39a4194778e3bd548f4532f951619a9921726
Thanu Shri <thanuuu89@gmail.com>
2025-09-15 22:26:38 +0530

    Update Bbee.py

--- Bbee.py
+++ Bbee.py
@@ -20,6 +20,9 @@ def predict():
                                amount=amt,
                                category=category)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+
 
